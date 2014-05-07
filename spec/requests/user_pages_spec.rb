144e430f55e29801450a749c235c87835f2c15a4
Jeremy Radant <radant@madisoncollege.edu>
2014-05-07 10:49:52 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -46,14 +46,13 @@ describe "User pages" do
         it { should have_content('error') }
       end
 
-      describe "after saving the user" do
+       describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
-
 
 
       it "should create a user" do
